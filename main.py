8741b32813d6e2e421dc932168f27c9e50c8fef0
Avenjuatchon <juatchon.dg@obmontessori.edu.ph>
2026-02-16 09:05:19 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from pyscript import display, document
+
+def check(e):
+     document.getElementById('username').innerHTML = ' '
+     password = document.getElementById('password').value
+     password_length = len(password)
+               
+if username is missing 7 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username is missing 6 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username is missing 5 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username is missing 4 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username is missing 3 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username is missing 2 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username is missing 1 characters
+     display("Username must atleast have 7 charaters and one letter")
+
+if username has 7 characters
+     display("Account created")
+else:
+    display("Account not created")
+        
+        
+    
